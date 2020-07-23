ae20064a017a049bd89528d8beeb3cfb416f2da2
haigen.zhu <haigen.zhu@tcl.com>
2020-07-23 14:17:14 +0800

    add audio

--- app/src/main/java/com/example/yinshipin/activity/VideoExtractActivity.kt
+++ app/src/main/java/com/example/yinshipin/activity/VideoExtractActivity.kt
@@ -43,14 +43,18 @@ class VideoExtractActivity : AppCompatActivity() {
             var videoTrackIndex: Int = -1
             var audioTrackIndex = -1
             mediaExtractor.setDataSource(VIDEO_PATH)
+            val fileOutputStream = FileOutputStream(VIDEO_OUTPUT_PATH)
+            val muxer =
+                MediaMuxer(fileOutputStream.fd, MediaMuxer.OutputFormat.MUXER_OUTPUT_MPEG_4)
             for (i in 0..mediaExtractor.trackCount) {
                 val trackFormat = mediaExtractor.getTrackFormat(i)
                 val mimeType = trackFormat.getString(MediaFormat.KEY_MIME)
+                //选择第一个遇到的videoTrack和第一个遇到的audioTrack
                 if (mimeType.startsWith("video/")) {
-                    videoTrackIndex = i
+                    if (videoTrackIndex == -1) videoTrackIndex = i
                     if (audioTrackIndex != -1) break
                 } else if (mimeType.startsWith("audio/")) {
-                    audioTrackIndex = i
+                    if (audioTrackIndex == -1) audioTrackIndex = i
                     if (videoTrackIndex != -1) break
                 }
             }
@@ -58,18 +62,19 @@ class VideoExtractActivity : AppCompatActivity() {
             val startTime = 2000000L
             val endTime = 20000000L
             var startPresentationTime = 0L
-            var startVideoPresentationTime = 0L
+            val mediaMetadataRetriever = MediaMetadataRetriever()
+            var bufferSize = -1
+            var videoDstTrack = -1
+            var audioDstTrack = -1
             if (videoTrackIndex != -1) {
                 mediaExtractor.selectTrack(videoTrackIndex)
                 mediaExtractor.seekTo(startTime, MediaExtractor.SEEK_TO_CLOSEST_SYNC)
                 startPresentationTime = mediaExtractor.sampleTime
                 val trackFormat = mediaExtractor.getTrackFormat(videoTrackIndex)
-                val fileOutputStream = FileOutputStream(VIDEO_OUTPUT_PATH)
-                val muxer =
-                    MediaMuxer(fileOutputStream.fd, MediaMuxer.OutputFormat.MUXER_OUTPUT_MPEG_4)
 //            fileOutputStream.close()
-                muxer.addTrack(trackFormat)
-                val mediaMetadataRetriever = MediaMetadataRetriever()
+                videoDstTrack = muxer.addTrack(trackFormat)
+
+                //获取视频的旋转角度
                 mediaMetadataRetriever.setDataSource(VIDEO_PATH)
                 val degreesString: String = mediaMetadataRetriever.extractMetadata(
                     MediaMetadataRetriever.METADATA_KEY_VIDEO_ROTATION
@@ -80,53 +85,87 @@ class VideoExtractActivity : AppCompatActivity() {
                         muxer.setOrientationHint(degrees)
                     }
                 }
-                var bufferSize = -1
+
                 if (trackFormat.containsKey(MediaFormat.KEY_MAX_INPUT_SIZE)) {
                     val newSize: Int = trackFormat.getInteger(MediaFormat.KEY_MAX_INPUT_SIZE)
                     bufferSize = if (newSize > bufferSize) newSize else bufferSize
                 }
-                if (bufferSize < 0) {
-                    bufferSize = 1024 * 1024
-                }
-                val dstBuf = ByteBuffer.allocate(bufferSize)
-                val bufferInfo = MediaCodec.BufferInfo()
 
-                try {
-                    muxer.start()
-                    while (isActive) {
-                        bufferInfo.size = mediaExtractor.readSampleData(dstBuf, 0)
-                        if (bufferInfo.size < 0) {
-                            Log.d("TAG", "size < 0")
-                            break
-                        }
-                        bufferInfo.offset = 0
-                        bufferInfo.presentationTimeUs = mediaExtractor.sampleTime
-                        if (bufferInfo.presentationTimeUs > endTime) {
-                            Log.d(
-                                "TAG",
-                                "end presentationTimeUs ->${bufferInfo.presentationTimeUs}"
-                            )
-                            break
-                        }
-                        bufferInfo.presentationTimeUs =
-                            bufferInfo.presentationTimeUs - startPresentationTime
-                        val isKeyFrame =
-                            mediaExtractor.sampleFlags and MediaExtractor.SAMPLE_FLAG_SYNC != 0
-                        val flags = if (isKeyFrame) MediaCodec.BUFFER_FLAG_KEY_FRAME else 0
-                        bufferInfo.flags = flags
-                        muxer.writeSampleData(videoTrackIndex, dstBuf, bufferInfo)
-                        mediaExtractor.advance()
-                    }
-                } catch (e: Exception) {
-                    Log.d("TAG", "write video error:$e")
-                } finally {
-                    mediaMetadataRetriever.release()
-                    mediaExtractor.release()
-                    muxer.stop()
-                }
             } else {
                 Log.d("TAG", "no video in $VIDEO_PATH")
 //                Toast.makeText(this, "no video in $VIDEO_PATH", Toast.LENGTH_LONG).show()
+            }
+
+            if (audioTrackIndex != -1) {
+                mediaExtractor.selectTrack(audioTrackIndex)
+                val trackFormat = mediaExtractor.getTrackFormat(audioTrackIndex)
+                audioDstTrack = muxer.addTrack(trackFormat)
+                if (trackFormat.containsKey(MediaFormat.KEY_MAX_INPUT_SIZE)) {
+                    val newSize: Int = trackFormat.getInteger(MediaFormat.KEY_MAX_INPUT_SIZE)
+                    bufferSize = if (newSize > bufferSize) newSize else bufferSize
+                }
+            } else {
+                Log.d("TAG", "no audio in $VIDEO_PATH")
+            }
+
+
+            val bufferInfo = MediaCodec.BufferInfo()
+            if (bufferSize < 0) {
+                bufferSize = 1024 * 1024
+            }
+            val dstBuf = ByteBuffer.allocate(bufferSize)
+            var startVideoPresentationTime = 0L
+            var startAudioPresentationTime = 0L
+            var firstVideoPacket = false
+            var firstAudioPacket = false
+            try {
+                muxer.start()
+                while (isActive) {
+                    bufferInfo.size = mediaExtractor.readSampleData(dstBuf, 0)
+                    if (bufferInfo.size < 0) {
+                        Log.d("TAG", "size < 0")
+                        break
+                    }
+                    bufferInfo.offset = 0
+                    bufferInfo.presentationTimeUs = mediaExtractor.sampleTime
+                    if (bufferInfo.presentationTimeUs > endTime) {
+                        Log.d(
+                            "TAG",
+                            "end presentationTimeUs ->${bufferInfo.presentationTimeUs}"
+                        )
+                        break
+                    }
+                    val isKeyFrame =
+                        mediaExtractor.sampleFlags and MediaExtractor.SAMPLE_FLAG_SYNC != 0
+                    val flags = if (isKeyFrame) MediaCodec.BUFFER_FLAG_KEY_FRAME else 0
+                    bufferInfo.flags = flags
+
+                    val sampleTrackIndex = mediaExtractor.sampleTrackIndex
+                    if (sampleTrackIndex == videoTrackIndex) {
+                        if (!firstVideoPacket) {
+                            firstVideoPacket = true
+                            startVideoPresentationTime = bufferInfo.presentationTimeUs
+                        }
+                        bufferInfo.presentationTimeUs =
+                            bufferInfo.presentationTimeUs - startVideoPresentationTime
+                        muxer.writeSampleData(videoDstTrack, dstBuf, bufferInfo)
+                    } else if (sampleTrackIndex == audioTrackIndex) {
+                        if (!firstAudioPacket) {
+                            firstAudioPacket = true
+                            startAudioPresentationTime = bufferInfo.presentationTimeUs
+                        }
+                        bufferInfo.presentationTimeUs =
+                            bufferInfo.presentationTimeUs - startAudioPresentationTime
+                        muxer.writeSampleData(audioDstTrack, dstBuf, bufferInfo)
+                    }
+                    mediaExtractor.advance()
+                }
+            } catch (e: Exception) {
+                Log.d("TAG", "write video error:$e")
+            } finally {
+                mediaMetadataRetriever.release()
+                mediaExtractor.release()
+                muxer.stop()
             }
         }
 
